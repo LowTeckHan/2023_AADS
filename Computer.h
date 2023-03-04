f66a93a39a9fd2ac60f324e86027380273d7f8fe
LowTeckHan <100545520+LowTeckHan@users.noreply.github.com>
2023-03-04 22:11:23 +1030

    Add files via upload

--- Computer.h
+++ Computer.h
@@ -1,11 +1,14 @@
 #ifndef Computer_H
 #define Computer_H
+#include "Player.h"
 #include<string> 
 
 using namespace std;
 
-class Computer :public Player
+class Computer : public Player
 {
+    private:
+
     public:
     string name_pc;
     char move_pc='R';
